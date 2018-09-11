6990a623fd1a06da313563703c52343ea09263a7
Denis Denisenko <denis@sparkequation.com>
2018-09-11 09:14:52 +0700

    Initial Commit

---
+++ basic-app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  basic-app
+//
+//  Created by Denis on 9/11/18.
+//  Copyright © 2018 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ basic-app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  basic-app
+//
+//  Created by Denis on 9/11/18.
+//  Copyright © 2018 Denis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
